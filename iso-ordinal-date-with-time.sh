1786c31a0afed837d709f59fa88a5d2db712ba1f
PeterCullenBurbery <peter.cullen.burbery@gmail.com>
2024-04-06 15:04:32 -0400

    added 2 date time scripts and z setting
    I added a script for the ISO week date, the ISO ordinal date, and a .zshrc for Kali Linux with aliases.

---
+++ iso-ordinal-date-with-time.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Parse key=value arguments with an optional -- prefix
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+   VALUE="${ARGUMENT#*=}"
+
+   # Remove the -- prefix from the key if present
+   [[ $KEY == --* ]] && KEY=${KEY:2}
+
+   export "$KEY"="$VALUE"
+done
+
+# Use the 'date' variable if provided, default to 'now' otherwise
+if [ ! -z "$date" ]; then
+    input_date="$date"
+else
+    input_date="now"
+fi
+
+# Format the date as ISO ordinal date with time
+formatted_date=$(date -d "$input_date" '+%Y-%j %H.%M.%S %Z')
+
+echo "$formatted_date"
